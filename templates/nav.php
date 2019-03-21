9426def6550f1df589fb7f9b09d7d2ce04da0611
exsersewo <exsersewo@systemexit.co.uk>
2019-03-21 01:50:06 +0000

    Add icons to navbar

--- templates/nav.php
+++ templates/nav.php
@@ -28,35 +28,51 @@ if(ieVersion(navigator.userAgent) !== undefined)
 <a class="navButton name" href="/">Skuld</a>
 <a class="burgerButton" href="javascript:void(0)" onclick="showMenu()"><i class="fa fa-bars"></i></a>
 <ul id="navButtons">
-<li class="navButton divNav"><a href="/commands">Commands</a></li>
-<li class="navButton divNav"><a href="/credits">Credits</a></li>
-<li class="navButton divNav ddMain" id="lbName">Leaderboard
+<li class="navButton divNav"><a href="/commands"><i class="fa fa-terminal"></i> Commands</a></li>
+<li class="navButton divNav"><a href="/credits"><i class="fa fa-users"></i> Credits</a></li>
+<li class="navButton divNav ddMain" id="lbName"><i class="fa fa-list-ol"></i> Leaderboard
 <div class="ddmenu">
-<a class="navButton" href="/leaderboard/money">Money</a>
+<a class="navButton" href="/leaderboard/money"><i class="fa fa-money-bill"></i> Money</a>
 </div>
 </li>
 <?php
 if(!$isLoggedIn)
 {
 ?>
-<li class="navButton"><a href="/login" rel="nofollow">Login</a>
+<li class="navButton"><a href="/login" rel="nofollow"><i class="fas fa-sign-in-alt"></i> Login</a>
 <?php
 }
 ?>
-<li class="navButton" ><a href="/stats">Stats</a></li>
+<li class="navButton" ><a href="/stats"><i class="fa fa-chart-line"></i> Stats</a></li>
 <?php
 if($isLoggedIn)
 {?>
-<li class="navButton ddMain"><a href="javascript:void(0)" id="profileName"><?=$usr->username?></a>
-<div class="ddmenu pMenu" style="right:0;">
-<a class="navButton" href="/dashboard" rel="nofollow">Dashboard</a>
-<a class="navButton" href="/profile" rel="nofollow">Profile</a>
-<a class="navButton" href="/logout" rel="nofollow">Logout</a>
+<li class="navButton ddMain"><a href="javascript:void(0)" onmouseenter="checkHover()" id="profileName"><?=$usr->username?></a>
+<div class="ddmenu pMenu">
+<a class="navButton" href="/dashboard" rel="nofollow"><i style="text-align:left;margin-left:5px;line-height:64px;" class="left fas fa-tachometer-alt"></i> Dashboard</a>
+<a class="navButton" href="/profile" rel="nofollow"><i style="text-align:left;margin-left:5px;line-height:64px;" class="left fas fa-user"></i> Profile</a>
+<a class="navButton" href="/logout" rel="nofollow"><i style="text-align:left;margin-left:5px;line-height:64px;" class="left fas fa-sign-out-alt"></i> Logout</a>
 </div>
 </li>
 <?php
 }
 ?>
 </ul>
+<script>
+let ddl = document.getElementsByClassName('ddmenu pMenu')[0];
+let bounding = null;
+
+function checkHover()
+{
+  bounding = ddl.getBoundingClientRect();
+  let rightOutside = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+  console.log(rightOutside);
+  if(rightOutside)
+  {
+    ddl.style.right = "0";
+  }
+}
+
+</script>
 </nav>
 <div id="navhandler" style="width:100%;height:100%;display:none;position:fixed;margin:0;padding:0;z-index:99;" onclick="hideMenu()"></div>
